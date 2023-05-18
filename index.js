479e2c953c30e0507c7dec83a89cca701a041494
Victor Matheus <111321282+victormmoreira98@users.noreply.github.com>
2023-05-18 20:21:40 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -23,5 +23,35 @@ app.get("/fatorial", (req, res)=> {    //nome de onde eu vou entrar
 app.use(cors())
 
 
+app.get("/superfatorial", (req, res)=> {
+
+    let n = parseInt(req.query.n)
+
+    res.status(200).send({
+        resposta : superFactorial(n).toString()
+    })
+})
 
 app.listen(8080)
+
+
+
+function factorial(x){
+    if(x<=1){
+        return 1
+    }
+    else{
+        return x * factorial(x-1)
+    }
+}
+
+
+//calculate superfactorial using recursion
+function superFactorial(x){
+    if(x<=1){
+        return 1
+    }
+    else{
+        return x * factorial(x-1) * superFactorial(x-1)
+    }
+}
